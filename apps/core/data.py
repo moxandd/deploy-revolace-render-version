d67b99b015f4dbfbc7255337c2ef37079701857c
moxandd <danya-kazakov@mail.ru>
2024-08-24 10:35:21 +0300

    final changes

--- apps/core/data.py
+++ apps/core/data.py
@@ -3,6 +3,9 @@ from django.db import models
 class Category(models.TextChoices):
     DESIGN = "1", "Design",
     MOVIES = "2", "Movies",
-    TRAVELLING = "3", "Travelling"
+    TRAVELLING = "3", "Travelling",
+    ANIMALS = "4", "Animals",
+    STORIES = "5", "Stories",
+    FOOD = "6", "Food"
     
 
